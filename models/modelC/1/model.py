28f227072adfe40b3a167e29d75b718a720078b8
Vadim Kantorov <vadimkantorov@gmail.com>
2023-12-27 18:20:52 +0100

    Update model.py

--- models/modelC/1/model.py
+++ models/modelC/1/model.py
@@ -17,6 +17,13 @@ class TritonPythonModel:
         responses = []
         for request in requests:
             in_numpy = pb_utils.get_input_tensor_by_name(request, "text_input").as_numpy()
+            assert np.object_ == in_numpy.dtype, 'in this demo, triton passes in a numpy array of size 1 with object_ dtype'
+            assert 1 == len(in_numpy), 'in this demo, only supporting a single string per inference request'
+            assert bytes == type(in_numpy.tolist()[0]), 'this object encapsulates a byte-array'
+            str1 = in_numpy.tolist()[0].decode('utf-8')
+            str2 = self.model_name + str1 + ' World'
+
+            out_numpy = np.array([str2.encode('utf-8')], dtype = np.object_)
             out_pb = pb_utils.Tensor("text_output", out_numpy)
             responses.append(pb_utils.InferenceResponse(output_tensors = [out_pb]))
         return responses
